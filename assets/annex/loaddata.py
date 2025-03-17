c0e04583e9de89b06edfc5b14bf2f1c0b3c5d63e
thanhtulearncode <thanh_tu.vo@insa-cvl.fr>
2025-03-17 22:34:24 +0700

    update .hex to .dat

--- assets/annex/loaddata.py
+++ assets/annex/loaddata.py
@@ -8,20 +8,21 @@ def save_hex_to_file(hex_data, output_file):
     with open(output_file, 'w') as hex_file:
         hex_file.write(hex_data)
 
-file_path = 'game_save6.dat'
+for i in range(8):
+    file_path = f"game_save{i}.dat"
 
-hex_representation = convert_to_hex(file_path)
+    hex_representation = convert_to_hex(file_path)
 
-output_file = file_path.replace('.dat', '.hex')
+    output_file = file_path.replace('.dat', '.hex')
 
-save_hex_to_file(hex_representation, output_file)
+    save_hex_to_file(hex_representation, output_file)
 
-with open("game_save6.hex", "r") as f:
-    hex_data = f.read().strip()  
+    with open(f"game_save{i}.hex", "r") as f:
+        hex_data = f.read().strip()  
 
-binary_data = bytes.fromhex(hex_data)
+    binary_data = bytes.fromhex(hex_data)
 
-with open("output.dat", "wb") as f:
-    pickle.dump(binary_data, f)
+    with open(f"output{i}.dat", "wb") as f:
+        pickle.dump(binary_data, f)
 
 
