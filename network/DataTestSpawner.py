815c4879b36dfc97dfd7d48586de632908ce7cd3
thanhtulearncode <thanh_tu.vo@insa-cvl.fr>
2025-03-18 20:29:00 +0700

    spawner test

---
+++ network/DataTestSpawner.py
@@ -0,0 +1,18 @@
+from backend.Units import Unit
+import Data
+
+import random
+
+def spawner_test():
+    start_x = random.randint(0, 10)
+    start_y = random.randint(0, 10)
+    
+    update_types = ["place_unit", "remove_unit", "move_unit", "place_building", "remove_building"]
+    update_type = random.choice(update_types)
+
+    object_id = random.randint(0, 10)
+    object_position = (start_x, start_y)
+    
+    package_header = f"{random.randint(0, 10)},update_type:{update_type},object_id:{object_id}position:{object_position}"
+    print(package_header)
+
