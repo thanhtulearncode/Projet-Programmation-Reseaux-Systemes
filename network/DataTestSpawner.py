f935482bf7c970621e26e748be9ce54d35a6b4cd
byn227 <vinh_tri.nguyen@insa-cvl.fr>
2025-03-18 16:17:40 +0100

    add csvcsv

--- network/DataTestSpawner.py
+++ network/DataTestSpawner.py
@@ -1,15 +1,32 @@
 import random
+import csv
 
 def spawner_test():
-    start_x = random.randint(0, 10)
-    start_y = random.randint(0, 10)
-    
+    start_x = str(random.randint(0, 10))
+    start_y = str(random.randint(0, 10))
     update_types = ["place_unit", "remove_unit", "move_unit", "place_building", "remove_building"]
     update_type = random.choice(update_types)
-
-    object_id = random.randint(0, 10)
-    object_position = (start_x, start_y)
-    
-    package_header = f"{random.randint(0, 10)},update_type:{update_type},object_id:{object_id}position:{object_position}"
+    object_id = str(random.randint(0, 10))
+    object_position = f"{start_x};{start_y}"
+    package_header = f"{random.randint(0, 10)};{update_type};{object_id};{start_x};{start_y}"
     return package_header
 
+class Spawner:
+    def __init__(self):
+        pass
+    def extract_package(self, row):
+
+        package_header = f"{row[0]};{row[1]};{row[2]};{row[3]};{row[4]}"
+        return package_header
+    
+    def process_csv(self, file_name):
+        with open(file_name, mode="r") as file:
+            reader = csv.reader(file, delimiter=';')
+            next(reader) 
+            
+            for row in reader:
+                package_header = self.extract_package(row)
+                print(package_header)
+
+spawner = Spawner()
+spawner.process_csv("spawner_test_output.csv")
